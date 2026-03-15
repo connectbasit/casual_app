c3abec40b700837529ad47ac96c67c1b3e929103
basit <basitali.connect@gmail.com>
2026-03-15 11:53:19 +0500

    Run First Test Case

--- lib/utils/custom_messages.dart
+++ lib/utils/custom_messages.dart
@@ -3,6 +3,7 @@ import '../index.dart';
 void customMessage(BuildContext context, String message, int statusCode){
   ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
+        key: Key('snackBar'),
           content: CustomText(text: message, size: 14, fontWeight: FontWeight.bold, textColor: Colors.white,),
         behavior: SnackBarBehavior.floating,
         padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
--- patrol_test/app_test.dart
+++ patrol_test/app_test.dart
@@ -1,6 +1,5 @@
+import 'package:casual_app/index.dart';
 import 'package:casual_app/main.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:patrol/patrol.dart';
 
 void main() {
@@ -10,17 +9,21 @@ void main() {
       await $.pumpWidgetAndSettle(const MyApp());
 
       await $(#comment).tap();
-
       await $(#CommentScreen).waitUntilVisible();
+      await $(ListTile).at(4).scrollTo().tap();
+      await $(#snackBar).waitUntilVisible();
 
       await $(#quote).tap();
-
       await $(#QuoteScreen).waitUntilVisible();
+      await $(ListTile).at(4).scrollTo().tap();
+      await $(#snackBar).waitUntilVisible();
 
       await $(#recipe).tap();
-
       await $(#RecipeScreen).waitUntilVisible();
+      await $(ListTile).at(1).scrollTo().tap();
+      await $(#snackBar).waitUntilVisible();
 
-    },
+
+        },
   );
 }
